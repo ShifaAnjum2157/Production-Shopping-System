36d92eab04a962030000c92a798d433d520a0489
ShifaAnjum2157 <shifaanjum6003@gmail.com>
2024-08-10 17:23:25 +0530

    update

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/autoRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+
+
+
 import cors from 'cors'
 dotenv.config()
 connectDB();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(cors())
@@ -18,10 +26,13 @@ app.use(morgan('dev'))
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product', productRoutes)
+app.use(express.static(path.join(__dirname, './client/build')));
+
 app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
 app.get('/', (req, res) => {
     res.send("<h1>E-commerce<h1/>")
 })
